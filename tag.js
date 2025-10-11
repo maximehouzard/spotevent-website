86bc802e2143f3c87ff6e2b403490d370c60638d
maximehouzard <maxime.houzard@spotevent.fr>
2025-10-11 19:57:54 +0200

    Update tag.js

--- tag.js
+++ tag.js
@@ -1,8 +1,14 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-18DZD2SESK"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+(function() {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-18DZD2SESK';
+  document.head.appendChild(script);
 
+  // Initialiser le dataLayer et la fonction gtag
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ window.dataLayer.push(arguments); }
+
+  // Configurer le tag
+  gtag('js', new Date());
   gtag('config', 'G-18DZD2SESK');
-</script>
+})();
