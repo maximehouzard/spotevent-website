08e9e71484cc569a0db677d4ad3a5a61ad2d460c
maximehouzard <maxime.houzard@spotevent.fr>
2025-10-11 19:54:05 +0200

    Create tag.js

---
+++ tag.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-18DZD2SESK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-18DZD2SESK');
+</script>
